9ac9141e72d86db4231198e090524e9054ca2a0e
bg0jr <bg0jr@shared-files.de>
2025-08-26 11:01:42 +0200

    obsolete coreui component removed

--- src/RaynMaker.Portfolio.WebUI/src/App.vue
+++ src/RaynMaker.Portfolio.WebUI/src/App.vue
@@ -1,33 +1,31 @@
 <template>
-  <CWrapper>
-    <CHeader fixed with-subheader light>
-      <CHeaderBrand style="padding: 5px" to="/"> RaynMaker </CHeaderBrand>
-      <CHeaderNav class="d-md-down-none mr-auto">
-        <CNavItem>
-          <router-link class="nav-link" to="/Positions">Positions</router-link>
-        </CNavItem>
-        <CNavItem>
-          <router-link class="nav-link" to="/Performance">Performance</router-link>
-        </CNavItem>
-        <CNavItem>
-          <router-link class="nav-link" to="/Cashflow">Cashflow</router-link>
-        </CNavItem>
-        <CNavItem>
-          <router-link class="nav-link" to="/ClosedPositions">History</router-link>
-        </CNavItem>
-      </CHeaderNav>
-    </CHeader>
+  <CHeader fixed with-subheader light>
+    <CHeaderBrand style="padding: 5px" to="/"> RaynMaker </CHeaderBrand>
+    <CHeaderNav class="d-md-down-none mr-auto">
+      <CNavItem>
+        <router-link class="nav-link" to="/Positions">Positions</router-link>
+      </CNavItem>
+      <CNavItem>
+        <router-link class="nav-link" to="/Performance">Performance</router-link>
+      </CNavItem>
+      <CNavItem>
+        <router-link class="nav-link" to="/Cashflow">Cashflow</router-link>
+      </CNavItem>
+      <CNavItem>
+        <router-link class="nav-link" to="/ClosedPositions">History</router-link>
+      </CNavItem>
+    </CHeaderNav>
+  </CHeader>
 
-    <div class="c-body">
-      <main class="c-main">
-        <CContainer fluid>
-          <transition name="fade" mode="out-in">
-            <router-view :key="$route.path"></router-view>
-          </transition>
-        </CContainer>
-      </main>
-    </div>
-  </CWrapper>
+  <div class="c-body">
+    <main class="c-main">
+      <CContainer fluid>
+        <transition name="fade" mode="out-in">
+          <router-view :key="$route.path"></router-view>
+        </transition>
+      </CContainer>
+    </main>
+  </div>
 </template>
 
 <script>
